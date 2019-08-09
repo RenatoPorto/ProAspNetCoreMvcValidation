25dbee3646bb2aa097eb6ef9931027b005b4eac6
Renato Rodrigues <renato_rodrigues2@yahoo.com.br>
2019-08-08 21:57:08 -0400

    Criaçao da classe Agenda e Compra

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ProAspNetCoreMvcValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProAspNetCoreMvcValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+
+        {
+            return View("Agenda", new Compromisso { Data = DateTime.Now });
+
+
+        }
+
+        [HttpPost]
+
+        public ViewResult Agenda(Compromisso compromisso)
+        {
+
+
+            if (ModelState.GetValidationState(nameof(compromisso.Data))
+       == ModelValidationState.Valid
+       && ModelState.GetValidationState(nameof(compromisso.NomeCliente))
+       == ModelValidationState.Valid
+       && compromisso.NomeCliente.Equals("Alice", StringComparison.OrdinalIgnoreCase)
+       && compromisso.Data.DayOfWeek == DayOfWeek.Monday)
+            {
+                ModelState.AddModelError("",
+                "Alice não pode ser agendada na segunda-feira");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View("Completo", compromisso);
+            }
+            else
+            {
+                return View();
+            }
+
+
+        }
+
+        [HttpGet]
+             public IActionResult Compra()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Compra(Compra compra)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("CompraConfirmado", compra);
+            }
+            else
+            {
+                return View(compra);
+            }
+        }
+
+
+    }
+
+}
---
+++ Models/Compromisso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAspNetCoreMvcValidation.Models
+{
+    public class Compromisso
+    {
+        public string NomeCliente { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public bool AceitaTermos { get; set; }
+   
+    }
+}
